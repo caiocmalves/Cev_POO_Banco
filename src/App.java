1b46ed7f0705b8c8df1f15e86521f80e40ffbf70
Caio Carvalho <caio.cmalves@gmail.com>
2023-04-14 18:50:35 -0300

    resolução da base do exercício

--- src/App.java
+++ src/App.java
@@ -1,5 +1,33 @@
+import classes.ContaBanco;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ContaBanco c1 = new ContaBanco();
+        System.out.println(c1.getStatus());
+        c1.abrirConta(01, "CC", "Caio Carvalho");
+        System.out.println(c1.getStatus());
+        c1.pagarMensal();
+        System.out.println(c1.getSaldo());
+        c1.depositar(150);
+        System.out.println(c1.getSaldo());
+        c1.sacar(88);
+        System.out.println(c1.getSaldo());
+
+        ContaBanco c2 = new ContaBanco();
+        c2.abrirConta(02, "CP", "DIOINZADA");
+        System.out.println(c2.getSaldo());
+        c2.pagarMensal();
+        System.out.println(c2.getSaldo());
+        
+
+
+        //c1.fecharConta();
+        System.out.println(c1.getSaldo());
+        c1.sacar(c1.getSaldo());
+        System.out.println(c1.getSaldo());
+        c1.fecharConta();
+        System.out.println(c1.getStatus());
+
+
     }
 }
--- src/classes/ContaBanco.java
+++ src/classes/ContaBanco.java
@@ -12,4 +12,86 @@ public class ContaBanco {
         this.saldo = 0;
     }
 
+    public void abrirConta(int numConta, String tipo, String dono) {
+        this.numConta = numConta;
+        this.tipo = tipo;
+        this.dono = dono;
+        this.status = true;
+        if (getTipo() == "CC") {
+            this.saldo += 50;            
+        } else {
+            this.saldo += 150;
+        }
+    }
+    
+    public void fecharConta(){
+        if (getSaldo() == 0 && getStatus() == true) {
+            this.status = false;
+            System.out.println("Sua conta foi fechada!");
+        }else {
+            System.out.println("Você não pode fechar sua conta!");
+        }
+    } 
+    
+    public void depositar(double valor) {
+        if (getStatus() == true) {
+            this.saldo += valor;
+            System.out.println("Depósito efetuado com sucesso!");
+        } else {
+            System.out.println("Sua conta está fechada, não foi possível executar o depósito!");
+        }
+    }
+    
+    public void sacar(double valor) {
+        if (getStatus() != true) {
+            System.out.println("Sua conta não está aberta, por isso, não podemos executar o saque.");
+        }
+        else if (getSaldo() < valor)
+            System.out.println("Seu saldo é inferior ao valor que pretende sacar!");
+        else {
+            this.saldo -= valor;
+            System.out.println("Seu saque foi efetuado!");
+        }
+    }
+
+    public void pagarMensal() {
+        if (getTipo() == "CC" && getStatus() == true) {
+            this.saldo -= 12;
+            System.out.println("A mensalidade foi debitada!");
+        }
+        else if(getTipo() == "CP" && getStatus() == true) {
+            this.saldo -= 20;
+            System.out.println("A mensalidade foi debitada!");
+        }
+    }
+    
+    public int getNumConta() {
+        return numConta;        
+    }
+
+    public void setNumConta(int numConta){
+        this.numConta = numConta;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public String getDono(){
+        return dono;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
 }
+
+
